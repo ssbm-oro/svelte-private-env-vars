5d422c029c0ad55aab4e5e7d3e51673487f1a94b
ssbmoro <69113632+ssbm-oro@users.noreply.github.com>
2022-08-17 16:44:06 -0400

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { PUBLIC_MY_NAME } from "$env/static/public";
+    import { MY_LUGGAGE_COMBO } from "$env/static/private";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>My name is {PUBLIC_MY_NAME} and don't tell anyone {MY_LUGGAGE_COMBO}</p>
